8505013f1f81d6d84a64c6f57a2a73b1471ea164
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2022-09-28 21:41:17 +0300

    create element of 4.1 HW

---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,18 @@
+import { TransactionItem } from "components/TransactionItem/TransactionItem";
+
+export const TransactionHistory = ({ props }) => {
+    return (<table>
+  <thead>
+    <tr>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+
+  <tbody>
+   {<TransactionItem/>}
+  </tbody>
+</table>);
+}
+
