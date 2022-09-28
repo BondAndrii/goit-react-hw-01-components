abcaf0742e2571f6e6a4726ea9392c2e3d1da890
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2022-09-28 16:20:08 +0300

    debag

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,6 @@
 // import PropTypes from 'prop-types';
 import { BlockStat } from 'components/BlockStat/BlockStat';
-import data from '../../data.json'
+
 
 export const Statistics = ({ title, stats}) => { 
     return (<section>
